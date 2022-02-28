7da0a5c9690cd041375d4f203df7b2ea49d91a50
JAZMINHERNANDEZRICARDO <3326570431220@ingenieria.usac.edu.gt>
2022-02-27 20:59:53 -0600

    se sube tarea

---
+++ tarea2/src/tarea2/Tarea2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea2;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Tarea2 {
+        
+        public static void main(String[] args) {
+        NewJFrame miVentana_login = new NewJFrame();
+        miVentana_login.show(true);
+        }
+        void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
